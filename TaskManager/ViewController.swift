b73b0641254f1a9bd9e992e95e99dbc10c4777d2
C1322374 Rafael Aragao Camara <c1322374@interno.bb.com.br>
2025-08-07 08:26:18 -0300

    First commit

---
+++ TaskManager/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TaskManager
+//
+//  Created by Rafael on 07/08/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .red
+    }
+
+
+}
+
---
+++ TaskManagerTests/TaskManagerTests.swift
@@ -0,0 +1,17 @@
+//
+//  TaskManagerTests.swift
+//  TaskManagerTests
+//
+//  Created by Rafael on 07/08/25.
+//
+
+import Testing
+@testable import TaskManager
+
+struct TaskManagerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}

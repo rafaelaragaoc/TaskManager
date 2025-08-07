31ce388121c9f6a7461f8a0433640f6b01b2e8b1
C1322374 Rafael Aragao Camara <c1322374@interno.bb.com.br>
2025-08-07 08:35:15 -0300

    Changing the UIKit home screen to a SwiftUIView home screen

---
+++ TaskManager/Home/HomeScreemView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeScreemView.swift
+//  TaskManager
+//
+//  Created by Rafael on 07/08/25.
+//
+
+import SwiftUI
+
+struct HomeScreemView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HomeScreemView()
+}
